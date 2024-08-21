6417b473e3c09d5c5167e14afb72c261243b9335
B2 <dantsthegamer17@gmail.com>
2024-08-21 13:56:58 -0500

    updated java notes

--- JavaPractice/Notes/HelloScanner.java
+++ JavaPractice/Notes/HelloScanner.java
@@ -1,8 +1,14 @@
 //package
 //import
+import java.util.Scanner;
 //ALWAYS REMEMBER - public class filename without the extension
 public class HelloScanner {
     public static void main(String[] args){
         System.out.println("What's for lunch?");
+        //any time I need user input... do this...
+        Scanner ui = new Scanner(System.in);
+        String food = ui.nextLine();    //get the next line of input from the user
+
+        System.out.println("Wow! "+food+" sounds Delish!" );
     }
 }
